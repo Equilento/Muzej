b77ee83495606bbb91a8831433c3aa957cc8f8d9
Domagoj Krajnovic <equilento@gmail.com>
2024-06-08 07:37:50 +0200

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2015094.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 183492471, 183492464, 183492473, 183492476, 183492478, 183492495, 183492462 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
